d2f9d44e797d43d8861e11a9ae0288bd0f975f3e
vssokolovaa <valesokolova@ozon.ru>
2022-04-02 18:39:36 +0300

    added markers

--- tests/test_one.py
+++ tests/test_one.py
@@ -1,10 +1,12 @@
+import pytest
 def summator(a, b):
     return a + b
 
 
+@pytest.mark.smoke
 def test_one():
     assert summator(10, 20) == 30
 
-
+@pytest.mark.regression
 def test_two():
     assert summator(10, 14) == 30
